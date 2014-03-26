5ae2ac396919b07305d6ff57aab3401a0591633d
tmidhat <tarikmidhat@gmail.com>
2014-03-27 00:18:19 +0100

    User Registration
    URI: /user/register
    
    Issues:
    1. Validation won't work to compare matching passwords, could be VS
    2013, MVC 5 incompatibilities
    
    2. Only adds user with id "0" to empty table, else: throws exception!
    Could be primary key issues with EF?

---
+++ LolFightProjekat/LolFightProjekat/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LolFightProjekat.Models;
+
+namespace LolFightProjekat.Controllers
+{
+    public class UserController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(korisnici U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (lolfightbazaEntities dc = new lolfightbazaEntities())
+                {
+                    dc.korisnicis.Add(U);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Registered succesfully!";
+                }
+            }
+            return View(U);
+        }
+    }
+}
