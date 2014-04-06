80ff1bb170ac10ed23dc6294c3ed340034cf9487
tmidhat <tarikmidhat@gmail.com>
2014-04-06 20:36:04 +0200

    LolFight: Phoenix Rising
    Recreated LolFight under LocalDB(SQL Server) instead of WAMP,
    implemented user registration, otherwise clean slate, we can reimplement
    already finished stuff. Ja ja!

---
+++ LolFight/LolFight/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LolFight.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(User U)
+        {
+            if (ModelState.IsValid)
+            {
+                using (LolContext dc = new LolContext())
+                {
+                    dc.Users.Add(U);
+                    dc.SaveChanges();
+                    ModelState.Clear();
+                    U = null;
+                    ViewBag.Message = "Registered! Get to LolFighting!";
+                }
+            }
+            return View(U);
+        }
+
+    }
+}
---
+++ LolFight/LolFight/Models/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace LolFight.Models
+{
+    public class Account
+    {
+        public int IdUser { get; set; }
+        [Required(ErrorMessage = "You know you need a username right?", AllowEmptyStrings = false)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "You need a password, make it strong!", AllowEmptyStrings = false)]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        [StringLength(50, MinimumLength = 9, ErrorMessage = "Let's make it at least 9 characters, to be safe..")]
+        public string Password { get; set; }
+        //[Compare("Password", ErrorMessage = "Confirm password dose not match.")]
+        [DataType(System.ComponentModel.DataAnnotations.DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public string eMail { get; set; }
+    }
+}
