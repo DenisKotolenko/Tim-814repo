657ac0b32a40a00b15b4575f7d8d0d59a025f939
NerminBarucija <nb15322@etf.unsa.ba>
2014-04-10 00:28:58 +0200

    dodan tab control i dropdown

--- LolFightProjekat/LolFightProjekat/App_Start/BundleConfig.cs
+++ LolFightProjekat/LolFightProjekat/App_Start/BundleConfig.cs
@@ -12,8 +12,8 @@ namespace LolFightProjekat
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
-
+                        "~/Scripts/bootstrap.js"));
+           
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
@@ -24,7 +24,9 @@ namespace LolFightProjekat
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+            "~/Content/bootstrap.css",
+            "~/Content/bootstrap-theme.css"));
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
                         "~/Content/themes/base/jquery.ui.resizable.css",
