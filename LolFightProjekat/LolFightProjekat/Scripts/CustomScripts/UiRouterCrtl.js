12d3e884666a8bea42109c63c634450f3a081eaf
DenisKotolenko <denis.kotolenko89@gmail.com>
2014-06-04 16:02:28 +0200

    uradjen ui router za user(home) i shop

--- LolFightProjekat/LolFightProjekat/Scripts/CustomScripts/UiRouterCrtl.js
+++ LolFightProjekat/LolFightProjekat/Scripts/CustomScripts/UiRouterCrtl.js
@@ -10,13 +10,51 @@ angular
   .config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
       $urlRouterProvider.otherwise('/');
       $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: '../PartialViews/Proba.html',
-           
+         .state('home', {
+          url: '/',
+          templateUrl: '../PartialViews/UserPartial/UserPartial.html'
+
         })
+
+        .state('shop', {
+            url: '/shop',
+            templateUrl: '../PartialViews/ShopPartial/ShopPartial.html',
+            controller: 'GetItemsCtrl'
+        })
+
 
   }])
 
 
+angular
+.module('app')
+.controller('GetItemsCtrl', function ($scope, $http) {
+   
+    $http.get('/api/ShopApi').success(function (data) {
+        $scope.Items = data;
+    });
 
+    $scope.BuyItem = function (Price) {
+        var i = $scope.names.indexOf(Price);
+        $scope.names.splice(i, 1);
+    };
+});
+
+
+/*
+angular
+  .module('app')
+  .controller('homeCtrl', ['$scope', 'Friends', function ($scope, Friends) {
+      $scope.title = "Home";
+      Friends.get().then(function (data) {
+          $scope.friends = data;
+      });
+      $scope.items = ['home', 'about', 'contact'];
+      $scope.selectedValue = 'home';
+
+      $scope.save = function () {
+          $http.post('/api/friends', friends);
+      };
+  }]);
+
+*/
